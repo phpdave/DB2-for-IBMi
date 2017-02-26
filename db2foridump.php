4d04c8c0c987612670f08c9ea0d8135ef50e862f
phpdave <davelhs03@yahoo.com>
2017-02-26 12:18:25 -0500

    Update db2foridump.php

--- db2foridump.php
+++ db2foridump.php
@@ -1,8 +1,16 @@
 <?php
+
 /**
  * Dumps DB2 for i data objects into SQL
  */
-class db2foridump
-{
+class db2foridump {
+    private $_db2connection;
+    
+    /**
+     * Pass in the db2connection resource from db2_connect()
+     */
+    public function __construct($db2connection) {
+        $this->_db2connection = $db2connection;
+    }
 
 }
